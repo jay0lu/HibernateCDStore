6454e82da098261374ab0a6fc706ac8d31c58b8d
jay0lu <luzheng1992@gmail.com>
2015-11-02 16:27:01 -0500

    session logout

---
+++ src/com/jwt/hibernate/controller/LogoutControllerServlet.java
@@ -0,0 +1,34 @@
+package com.jwt.hibernate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+
+@WebServlet("/LogoutControllerServlet")
+public class LogoutControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+
+			HttpSession session = request.getSession();
+
+			session.setAttribute("sessionId", "");
+			response.sendRedirect("index.jsp");
+
+			System.out.println("Test get session:" + session.getAttribute("sessionId"));
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+}
