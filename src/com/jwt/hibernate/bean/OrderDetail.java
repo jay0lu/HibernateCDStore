bc531698dec1d7b6cf6a90a4c5d75679c6cdbdc8
jay0lu <luzheng1992@gmail.com>
2015-11-03 18:35:47 -0500

    fix entity error
    hibernate annotation fixed.
    part of encrypt function

--- src/com/jwt/hibernate/bean/OrderDetail.java
+++ src/com/jwt/hibernate/bean/OrderDetail.java
@@ -3,17 +3,13 @@ package com.jwt.hibernate.bean;
 import java.math.BigDecimal;
 import java.sql.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import org.hibernate.annotations.Columns;
+import javax.persistence.*;
 
  
 @Entity
+@Table(name="OrderDetail")
 public class OrderDetail {
-	@Id @GeneratedValue
+	
 	private int id;
     
 	private String userName;
@@ -23,37 +19,48 @@ public class OrderDetail {
     private String phoneNumber;
     private String address;
     
-    
+    @Id @GeneratedValue
+    @Column(name="id", nullable=false)
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+    @Column(name="userName", nullable=false)
 	public String getUserName() {
 		return userName;
 	}
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
+    @Column(name="orderDetail", nullable=false)
 	public String getOrderDetail() {
 		return orderDetail;
 	}
 	public void setOrderDetail(String orderDetail) {
 		this.orderDetail = orderDetail;
 	}
+	
+    @Column(name="date", nullable=false)
 	public String getDate() {
 		return date;
 	}
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+    @Column(name="price", nullable=false)
 	public BigDecimal getPrice() {
 		return price;
 	}
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
+	
+    @Column(name="phoneNumber", nullable=false)
 	public String getPhoneNumber()
 	{
 		return phoneNumber;
@@ -62,6 +69,8 @@ public class OrderDetail {
 	{
 		this.phoneNumber=phoneNumber;
 	}
+	
+    @Column(name="address", nullable=false)
 	public String getAddress()
 	{
 		return address;
